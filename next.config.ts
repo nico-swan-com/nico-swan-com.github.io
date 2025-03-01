27609132cbfd910ebf411751feae05d0f2332d09
Nico Swan <hi@nicoswan.com>
2025-03-01 10:43:36 +0200

    fix: :green_heart: fix the build config for building static site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const { BUILD_TYPE } = process.env
+
+const defaultConfig: NextConfig = {
+  distDir: '_next'
+}
+
+const staticConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true
+  }
+}
+
+let nextConfig = defaultConfig
+switch (String(BUILD_TYPE).toUpperCase()) {
+  case 'STATIC':
+    nextConfig = staticConfig
+    break
+  default:
+    nextConfig = defaultConfig
+    break
 }
 
 export default nextConfig
